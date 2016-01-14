6c8221546989c3f0b79fd57d7d7586c1bca61ba3
Li Ge <lige@yahoo-inc.com>
2016-01-13 17:47:48 -0800

    scalefold go-mt, not working

--- Makefile
+++ Makefile
@@ -13,18 +13,21 @@ JS_ST=js_knn_bf_st
 CPP_ST=cpp_knn_bf_st
 CPP_MT=cpp_knn_bf_mt
 GO_ST=go_knn_bf_st
+GO_MT=go_knn_bf_mt
 
 OUTPUT_PATH=$(MAKEFILE_PATH)output
 OUT_JS_ST=$(OUTPUT_PATH)/$(JS_ST)_r$(N_RECORDS)_q$(N_QUERIES)_k$(TOPK).txt
 OUT_CPP_ST=$(OUTPUT_PATH)/$(CPP_ST)_r$(N_RECORDS)_q$(N_QUERIES)_k$(TOPK).txt
 OUT_CPP_MT=$(OUTPUT_PATH)/$(CPP_MT)_r$(N_RECORDS)_q$(N_QUERIES)_k$(TOPK).txt
 OUT_GO_ST=$(OUTPUT_PATH)/$(GO_ST)_r$(N_RECORDS)_q$(N_QUERIES)_k$(TOPK).txt
+OUT_GO_MT=$(OUTPUT_PATH)/$(GO_MT)_r$(N_RECORDS)_q$(N_QUERIES)_k$(TOPK).txt
 
 EXE_PATH=$(MAKEFILE_PATH)exes
 EXE_JS_ST=$(EXE_PATH)/$(JS_ST)
 EXE_CPP_ST=$(EXE_PATH)/$(CPP_ST)
 EXE_CPP_MT=$(EXE_PATH)/$(CPP_MT)
 EXE_GO_ST=$(EXE_PATH)/$(GO_ST)
+EXE_GO_MT=$(EXE_PATH)/$(GO_MT)
 
 COPY=cp
 CHMOD=chmod
@@ -58,6 +61,9 @@ $(EXE_CPP_MT): $(CPP_MT).cpp
 $(EXE_GO_ST): $(GO_ST).go
 	$(GO) build $(GOFLAGS) -o $@ $^
 
+$(EXE_GO_MT): $(GO_MT).go
+	$(GO) build $(GOFLAGS) -o $@ $^
+
 $(OUT_JS_ST): $(INPUT_DATA) $(EXE_JS_ST)
 	time $(EXE_JS_ST) $(RECORDS) $(QUERIES) $(TOPK) > $@
 
@@ -70,6 +76,9 @@ $(OUT_CPP_MT): $(INPUT_DATA) $(EXE_CPP_MT)
 $(OUT_GO_ST): $(INPUT_DATA) $(EXE_GO_ST)
 	time $(EXE_GO_ST) $(RECORDS) $(QUERIES) $(TOPK) > $@
 
+$(OUT_GO_MT): $(INPUT_DATA) $(EXE_GO_MT)
+	time $(EXE_GO_MT) $(RECORDS) $(QUERIES) $(TOPK) > $@
+
 INPUT_DATA+=$(RECORDS)
 INPUT_DATA+=$(QUERIES)
 input: $(INPUT_DATA)
@@ -78,17 +87,20 @@ EXES+=$(EXE_JS_ST)
 EXES+=$(EXE_CPP_ST)
 EXES+=$(EXE_CPP_MT)
 EXES+=$(EXE_GO_ST)
+EXES+=$(EXE_GO_MT)
 exes: $(EXES)
 
 OUTPUT_DATA+=$(OUT_JS_ST)
 OUTPUT_DATA+=$(OUT_CPP_ST)
 OUTPUT_DATA+=$(OUT_CPP_MT)
 OUTPUT_DATA+=$(OUT_GO_ST)
+OUTPUT_DATA+=$(OUT_GO_MT)
 output: $(OUTPUT_DATA)
 out-js-st: $(OUT_JS_ST)
 out-cpp-st: $(OUT_CPP_ST)
 out-cpp-mt: $(OUT_CPP_MT)
 out-go-st: $(OUT_GO_ST)
+out-go-mt: $(OUT_GO_MT)
 
 check-js-st: $(OUT_JS_ST) $(OUT_CPP_ST)
 	diff $(OUT_JS_ST) $(OUT_CPP_ST)
@@ -99,7 +111,10 @@ check-cpp-mt: $(OUT_CPP_MT) $(OUT_CPP_ST)
 check-go-st: $(OUT_GO_ST) $(OUT_CPP_ST)
 	diff $(OUT_GO_ST) $(OUT_CPP_ST)
 
-check-all: check-js-st check-cpp-mt check-go-st
+check-go-mt: $(OUT_GO_ST) $(OUT_CPP_MT)
+	diff $(OUT_GO_MT) $(OUT_CPP_ST)
+
+check-all: check-js-st check-cpp-mt check-go-st check-go-mt
 
 clean-exes:
 	rm -fr $(EXE_PATH)/*
---
+++ go_knn_bf_mt.go
@@ -0,0 +1,165 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"bufio"
+	"math"
+	"strconv"
+	"github.com/petar/GoLLRB/llrb"
+)
+
+type Candidate struct {
+	record, distance float64
+	index uint64
+}
+
+func (self Candidate) Less(other llrb.Item) bool {
+	if (self.distance == other.(Candidate).distance) {
+		return self.index < other.(Candidate).index;
+	}
+	return self.distance < other.(Candidate).distance
+}
+
+type CandidateSet struct {
+	rbTree *llrb.LLRB
+}
+
+func createCandidateSet() *CandidateSet {
+	return &CandidateSet{llrb.New()}
+}
+
+func (s *CandidateSet) Len() uint8 {
+	return uint8(s.rbTree.Len())
+}
+
+func (s *CandidateSet) Insert(c Candidate) {
+	s.rbTree.ReplaceOrInsert(c)
+}
+
+func (s *CandidateSet) RemoveLast() Candidate {
+	return s.rbTree.DeleteMax().(Candidate)
+}
+
+func (s * CandidateSet) ToSlice() []Candidate {
+	candidates := []Candidate{}
+	t := s.rbTree
+	first := t.Min()
+	t.AscendGreaterOrEqual(first, func(item llrb.Item) bool {
+		candidate := item.(Candidate)
+		candidates = append(candidates, candidate)
+		return true
+	})
+	return candidates
+}
+
+func (s * CandidateSet) Print() {
+	vec := s.ToSlice()
+	for _, candidate := range vec {
+		fmt.Println(candidate.record, "\t", candidate.distance)
+	}
+}
+
+func readNumbers(filename string) []float64 {
+	src, _ := os.Open(filename)
+	defer src.Close()
+
+	nums := []float64{}
+	scanner := bufio.NewScanner(src)
+	for scanner.Scan() {
+		num, _ := strconv.ParseFloat(scanner.Text(), 64)
+		nums = append(nums, num)
+	}
+	return nums
+}
+
+func writeNumbers(nums []float64, writer *bufio.Writer) {
+	for _, num := range nums { fmt.Fprintln(writer, num) }
+}
+
+func knnBFSearchRoutine(
+	query float64,
+	records []float64,
+	topK uint8,
+  startIndex uint64,
+  endIndex uint64,
+  output chan []Candidate) {
+	s := createCandidateSet()
+	for i, record := range records[startIndex:endIndex+1] {
+		distance := math.Abs(record - query)
+		s.Insert(Candidate{record, distance, startIndex + uint64(i)})
+		if s.Len() > topK { s.RemoveLast() }
+	}
+  output <- s.ToSlice()
+}
+
+func knnBFSearch(
+	query float64,
+	records []float64,
+	topK uint8) []Candidate {
+  resultChan := make(chan []Candidate)
+  numGoRoutines := uint64(10)
+  numRecords := uint64(len(records))
+  base := numRecords / numGoRoutines
+  remains := numRecords % numGoRoutines
+
+  startIndex := uint64(0)
+  for startIndex < numGoRoutines {
+    endIndex := startIndex + base - 1
+    if remains > 0 {
+      endIndex += 1
+      remains -= 1
+    }
+    go knnBFSearchRoutine(
+      query,
+      records,
+      topK,
+      startIndex,
+      endIndex,
+      resultChan)
+  }
+
+  s := createCandidateSet()
+
+  for candidates := range resultChan {
+    for _, candidate := range candidates {
+      s.Insert(candidate)
+      if s.Len() > topK { s.RemoveLast() }
+    }
+  }
+
+  return s.ToSlice()
+}
+
+func printUsageAndExit() {
+	fmt.Println("Usage: go_knn_bf_mt <datasetFile> <queriesFile> <topK>")
+	fmt.Println("	 Foreach query in <queriesFile> print <topK> nearest neighbor")
+	fmt.Println("	 in <datasetFile> and print to stdout.")
+	os.Exit(1);
+}
+
+func main() {
+	argc := len(os.Args)
+	if argc < 4 {
+		printUsageAndExit()
+	}
+
+	records := readNumbers(os.Args[1])
+	queries := readNumbers(os.Args[2])
+	topK, _ := strconv.ParseUint(os.Args[3], 10, 8)
+
+	var count uint64 = 0
+	for _, query := range queries {
+		count += 1
+		fmt.Fprintln(os.Stderr, "processing", count, ":", query)
+
+		candidates := knnBFSearch(query, records, uint8(topK))
+
+		fmt.Printf("%.6f:", query)
+		for i, candidate := range candidates {
+			if i > 0 { fmt.Print(",") }
+			fmt.Print(candidate.index)
+		}
+		fmt.Println()
+	}
+}
